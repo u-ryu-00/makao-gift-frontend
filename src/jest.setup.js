17785e7c700bab50f895d38277a06b66de5ed228
u-ryu-00 <lwh76104@gmail.com>
2023-06-05 08:29:13 +0900

    BackdoorAPI

---
+++ src/jest.setup.js
@@ -0,0 +1,15 @@
+import { cleanup } from '@testing-library/react';
+import server from './testServer';
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+
+afterAll(() => {
+  server.close();
+});
